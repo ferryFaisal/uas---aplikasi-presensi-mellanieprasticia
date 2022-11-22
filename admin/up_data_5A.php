6c1b7b7a972d40dd024b7208f7fa98c9f14fdfb8
mellanie <mellanieprasticiay@gmail.com>
2022-11-22 23:30:03 +0700

    belum selesai

---
+++ admin/up_data_5A.php
@@ -0,0 +1,20 @@
+<?php
+require('connect.php');
+  $nama   = $_POST['nama'];
+  $nim     = $_POST['nim'];
+  $kelas         = $_POST['kelas'];
+  // $gambar_produk = $_FILES['gambar_produk']['name'];
+  $dc       = date("date_created");
+  $dm   = $dc;
+                  
+  $query = "INSERT INTO mahasiswa (nama, nim, kelas) VALUES ('$nama', '$nim', '$kelas')";
+                  $result = mysqli_query($conn, $query);
+                  
+                  if(!$result){
+                      die ("Query gagal dijalankan: ".mysqli_errno($conn).
+                           " - ".mysqli_error($conn));
+                  } else {
+                    echo "<script>alert('Data berhasil ditambah.');window.location='data_mhs_5A.php';</script>";
+                  }
+
+  
